69f285770ce9f5ed769fb2bcf91bbeae7703851f
Syed Jawad Shah <syedjawadshah00@gmail.com>
2024-02-06 19:42:37 +0500

    Javascript error function fixed

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function getWeatherData(cityValue) {
     weatherDataEl.querySelector(".icon").innerHTML = "";
     weatherDataEl.querySelector(".temperature").textContent = "";
     weatherDataEl.querySelector(".description").textContent =
-      "An error happened, please try again later";
+      "An error happened please try again later";
 
     weatherDataEl.querySelector(".details").innerHTML = "";
   }
